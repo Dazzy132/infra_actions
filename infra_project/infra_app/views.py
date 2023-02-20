1a859d79b3ea84c3de48460560de3be6bd22b4ef
Влад Соловьёв <vladsolo23rus@gmail.com>
2023-02-20 14:57:06 +0300

    Тест отправки на боевой сервер

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось изменить на боевом сервере!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
     return HttpResponse('А это вторая страница!')
+
+def test_page(request):
+    return HttpResponse('На боевом проекте изменения прошли')
